28f05088b025b69a6399e8934ea95b7dda25eae8
marionastro <marionastro65@gmail.com>
2024-04-30 14:24:29 +0200

    Commit test

--- src/main/java/it/unisa/superir/Main.java
+++ src/main/java/it/unisa/superir/Main.java
@@ -1,11 +1,23 @@
 package it.unisa.superir;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
+        VBox vBox = new VBox();
+        Text text = new Text("Hello World!");
+
+        vBox.getChildren().add(text);
+        Scene scene = new Scene(vBox);
+
+        primaryStage.setWidth(600);
+        primaryStage.setHeight(400);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
